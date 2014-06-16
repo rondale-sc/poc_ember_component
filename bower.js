700bdbbe9918a49b34d8f0899f0ef787f551e373
Jonathan Jackson <jonathan.jackson1@me.com>
2014-06-16 14:32:32 -0400

    CHANGE

--- bower.js
+++ bower.js
@@ -1,11 +1,1 @@
-{
-  "name": "ember",
-  "version": "1.7.0-beta.1+canary.f7db4bd2",
-  "main": [
-    "./ember.js"
-  ],
-  "dependencies": {
-    "jquery": ">= 1.7.0 <= 2.1.0",
-    "handlebars": ">= 1.0.0 < 2.0.0"
-  }
-}
+blah
